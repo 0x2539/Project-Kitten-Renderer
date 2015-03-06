8857260d802d05bbda7adf47a60838960fd85cb6
alexbuicescu <alexbuicescu@gmail.com>
2015-03-06 22:14:44 +0200

    updated code for sdl_platform.h; moved sdl window creation to static method

--- Renderer/BasicWindow.h
+++ Renderer/BasicWindow.h
@@ -1,6 +1,18 @@
 #ifndef BASICWINDOW_H_INCLUDED
 #define BASICWINDOW_H_INCLUDED
 
+//Using SDL and standard IO
+#include <SDL.h>
+#include <stdio.h>
 
+class BasicWindow
+{
+//Screen dimension constants
+static const int SCREEN_WIDTH = 640;
+static const int SCREEN_HEIGHT = 480;
+
+public:
+    static void initWindow();
+};
 
 #endif // BASICWINDOW_H_INCLUDED
